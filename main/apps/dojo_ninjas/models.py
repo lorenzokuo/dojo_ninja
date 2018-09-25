6ebd1cf9447ee73e438b09d58b8ff68f8d65eead
Lorenzokuo <lorenzokuo@gmail.com>
2018-09-24 19:08:21 -0700

    first commit

---
+++ main/apps/dojo_ninjas/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+from django.db import models
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    def __repr__(self):
+        return "<Blog object: {} {}>".format(self.name, self.city, self.state)
+
+class Ninja(models.Model):
+    dojo = models.ForeignKey(Dojo, related_name="ninjas")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
